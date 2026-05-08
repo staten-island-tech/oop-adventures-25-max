6ba0627e14299a2d5c6bb4febccaa6f8cae5362b
thomass238 <thomass238@nycstudents.net>
2026-05-08 10:05:02 -0400

    Update dsf.py

--- dsf.py
+++ dsf.py
@@ -1,15 +1,38 @@
 import time
-animatronic = False
+import random
+
+
+
+rand = random.randint(1, 4)
+time.sleep(10)
+    if rand == 1:
+        animatronic = True
+    else:
+        animatronic = False
+
+    if animatronic == True:
+        print ("ohmy god theyre gonna get me")
+    elif animatronic == False:
+        print("nothing happened"):
+
 print("There are inturders and now they're inside your house. Stay alive in your bedroom until sunrise to survive. they will activitly search for blUd. movement will increase his awareness ")
 print("everytime you open the flashlight their awarness will increase. if you keep hiding in your closet, it has a higher chance the pone ring which increase the chance of them comin")
 print("Frequently check each camera to spot them")
-choice = int(input("Where would you like to check? :Doorway(1): :Closet(2): :Under your bed(3):"))
-while animatronic == False:
+choice = int(input("Where would you like to check? :Doorway(1): :Closet(2): :Under your bed(3):     "))
+if choice == "1":
+    if animatronic == True:
+        input("aaahhh quick use flashlight ")
+    elif animatronic == False:
+        input("nothing here. go back? (Y/N)")
+
+
+
+while animatronic == True:
     start = time.time()
     while True:
         elapsed = time.time() - start
         
-        if elapsed >= 30:
+        if elapsed >= 10:
             break
 
 
@@ -17,8 +40,3 @@ while animatronic == False:
 
     print("\nStopped after 10 seconds.")
 
-if choice == "1":
-    if animatronic == True:
-        input("aaahhh quick use flashlight ")
-    else:
-        input("nothing here. go back? (Y/N)")
